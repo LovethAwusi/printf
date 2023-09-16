ef5c3daa7010f6b35276d1de5d04db1ab35ba2bd
LovethAwusi <awusieloho@gmail.com>
2023-09-16 15:13:21 -0700

    update

---
+++ print_1-digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * init_params - Initialize parameters struct.
+ * @params: The parameters struct to initialize.
+ * @ap: The argument pointer (unused).
+ */
+void init_params(params_t *params, va_list ap)
+{
+    // Clear all struct fields.
+    params->unsign = params->plus_flag = params->space_flag = params->hashtag_flag = 0;
+    params->zero_flag = params->minus_flag = params->h_modifier = params->l_modifier = 0;
+
+    // Reset width and precision.
+    params->width = 0;
+    params->precision = UINT_MAX;
+
+    (void)ap; // Avoid unused argument warning.
+}
+
